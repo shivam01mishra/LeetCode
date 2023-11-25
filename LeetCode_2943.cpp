dc6608a3f94cb11d4356138fc4cfef70407b6b5d
Shivam Mishra <65381465+shivammishra160499@users.noreply.github.com>
2023-11-25 22:43:49 +0530

    Add files via upload

--- LeetCode_2943.cpp
+++ LeetCode_2943.cpp
@@ -1,34 +1,19 @@
 class Solution {
 public:
-    int maximizeSquareHoleArea(int n, int m, vector<int>& h, vector<int>& v) {
-        sort(h.begin(),h.end());
-        sort(v.begin(),v.end());
-        int max1=1;
-        
-        int count=0;
-        for(int i=0;i<h.size();i++)
-        {
-           if(i==0 or h[i]!=h[i-1]+1)
-               count=2;
-            else
-            count++;
-            max1=max(max1,count);
-        }
-        
-          int max2=1;
-        
-        
-        for(int i=0;i<v.size();i++)
-        {
-           if(i==0 or v[i]!=v[i-1]+1)
-               count=2;
-            else
-            count++;
-            max2=max(max2,count);
-        }
-        //cout<<max1<<" "<<max2<<endl;
-        return pow(min(max1,max2),2);
-        
-        
+    int dp[1001][1001];
+    int fun(int i,int d,vector<int>& prices)
+    {
+        if(i==prices.size())
+            return 0;
+        if(dp[i][d]!=-1)
+            return dp[i][d];
+        if(d==0)
+        return dp[i][d]=fun(i+1,i+1,prices)+prices[i];
+            
+        return dp[i][d]=min(fun(i+1,d-1,prices),fun(i+1,i+1,prices)+prices[i]);
+    }
+    int minimumCoins(vector<int>& prices) {
+        memset(dp,-1,sizeof dp);
+        return fun(0,0,prices);
     }
 };
