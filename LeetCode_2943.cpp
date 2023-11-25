1186f883a710d63b73cc23aa32426c05b13fccae
Shivam Mishra <65381465+shivammishra160499@users.noreply.github.com>
2023-11-25 22:50:48 +0530

    Update LeetCode_2943.cpp

--- LeetCode_2943.cpp
+++ LeetCode_2943.cpp
@@ -3,32 +3,31 @@ public:
     int maximizeSquareHoleArea(int n, int m, vector<int>& h, vector<int>& v) {
         sort(h.begin(),h.end());
         sort(v.begin(),v.end());
-        int max1=1;
+         int max1=1;
         
-        int count=0;
-        for(int i=0;i<h.size();i++)
+        int count=1;
+        for(int i=1;i<h.size();i++)
         {
-           if(i==0 or h[i]!=h[i-1]+1)
-               count=2;
+           if( h[i]==h[i-1]+1)
+               count++;
             else
-            count++;
+            count=1;
             max1=max(max1,count);
         }
         
           int max2=1;
+          count=1;
         
-        
-        for(int i=0;i<v.size();i++)
+        for(int i=1;i<v.size();i++)
         {
-           if(i==0 or v[i]!=v[i-1]+1)
-               count=2;
+           if( v[i]==v[i-1]+1)
+               count++;
             else
-            count++;
+            count=1;
             max2=max(max2,count);
         }
         //cout<<max1<<" "<<max2<<endl;
-        return pow(min(max1,max2),2);
-        
+        return pow(min(max1+1,max2+1),2);
         
     }
 };
