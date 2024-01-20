21935fe95817ebfc79af7014299b3f1bcc687b84
Shivam Mishra <65381465+shivammishra160499@users.noreply.github.com>
2024-01-20 23:04:02 +0530

    Update 3012_leetcode.cpp

--- 3012_leetcode.cpp
+++ 3012_leetcode.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int minimumArrayLength(vector<int>& nums) {
         map<int,int>mp;
-        int m=0;
+        
         int n=nums.size();
         for(int i=0;i<n;i++)
         {
           mp[nums[i]]++;
-            m=max(m,mp[nums[i]]);
+           
         }
        int x = (*min_element(nums.begin(), nums.end()));
         for(int i:nums)
